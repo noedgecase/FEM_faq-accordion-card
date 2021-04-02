c31fd10855a98ae0e1f2e2ee7f867ff2b4b011b3
pillkaris <dragos042@yahoo.com>
2021-04-02 21:34:49 +0300

    it works now

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-'use strict'
-
 const questions = document.querySelectorAll('.question-line');
 const answers = document.querySelectorAll('.answer');
 
 questions.forEach((question, index) => {
-      question.addEventListener('click', function(){
-          answers.item(index).classList.toggle('hidden');
-      })
+  question.addEventListener('click', function () {
+    if (!answers.item(index).classList.contains('hidden')) {
+      answers.item(index).classList.add('hidden');
+    } else {
+      for (let i = 0; i < answers.length; i++) {
+        answers[i].classList.add('hidden');
+      }
+      answers.item(index).classList.remove('hidden');
+    }
   });
+});
